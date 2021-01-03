58de87405304d0a7ff75517a46e61a318dfa13a8
Heydrich_B <56252029+HeydrichBeillschmidt@users.noreply.github.com>
2021-01-03 15:22:09 +0800

    Support FPGA

--- stall_bus.v
+++ stall_bus.v
@@ -1,7 +1,4 @@
 module STALL_BUS(
-    input wire rdy_in,
-    input wire io_buffer_full,
-
     input wire req_if,
     input wire req_id,
     input wire req_mem,
@@ -10,7 +7,7 @@ module STALL_BUS(
 );
 
     always @(*) begin
-        if (~rdy_in | io_buffer_full | req_mem) stall_level = `Stall_All;
+        if (req_mem) stall_level = `Stall_All;
         else if (req_id) stall_level = `Stall_Issue;
         else if (req_if) stall_level = `Stall_Decode;
         else stall_level = `Stall_Null;
